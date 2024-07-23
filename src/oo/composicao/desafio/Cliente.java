3f4d053606ff39541a43ed41ef0b0c8a793d985a
lara.encoding <analara.cafe@gmail.com>
2024-07-23 14:43:21 +0100

    atualizando desafio composição.

--- src/oo/composicao/desafio/Cliente.java
+++ src/oo/composicao/desafio/Cliente.java
@@ -4,7 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
- 
-	List<Compra> compras = new ArrayList<>();
 	
+	final String nome;
+	final List<Compra> compras = new ArrayList<>();
+	
+	Cliente(String nome){
+		this.nome = nome;
+	}
+	
+	void adicionarCompra(Compra compra) {
+		this.compras.add(compra);
+	}
+	
+	double obterValorTotal() {
+		double total = 0;
+		
+		for(Compra compra: compras) {
+			total += compra.obterValorTotal();
+			
+		}
+		
+		return total;
+	}
 }
--- src/oo/composicao/desafio/Compra.java
+++ src/oo/composicao/desafio/Compra.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 public class Compra {
 
-	List<Item> itens = new ArrayList<>();
+	final List<Item> itens = new ArrayList<>();
 	
 	void adicionarItem(Produto p, int qtde) {
-		this.itens.add(new Item (p, qtde));
+		this.itens.add(new Item (p, qtde)); 
+	}
+	void adicionarItem(String nome, double preco, int qtde) {
+		var produto = new Produto(nome, preco);
+		this.itens.add(new Item (produto, qtde)); 
 	}
 	
 	double obterValorTotal() {
