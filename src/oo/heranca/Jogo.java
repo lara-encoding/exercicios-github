606d5c1d442722833b287c7d94a577098c59f0db
lara.encoding <analara.cafe@gmail.com>
2024-07-24 19:11:32 +0100

    atualizando o estudo herança 01.

--- src/oo/heranca/Jogo.java
+++ src/oo/heranca/Jogo.java
@@ -4,11 +4,11 @@ public class Jogo {
 	
 	public static void main(String[] args) {
 		
-		Jogador j1 = new Jogador();
+		Monstro j1 = new Monstro();
 		j1.x = 10;
 		j1.y = 10;
 		
-		Jogador j2 = new Jogador();
+		Heroi j2 = new Heroi();
 		j2.x = 10;
 		j2.y = 11;
 
@@ -16,6 +16,7 @@ public class Jogo {
 		System.out.println(j2.vida);
 		
 		j1.atacar(j2);
+		j2.atacar(j1);
 		
 		System.out.println(j1.vida);
 		System.out.println(j2.vida);
