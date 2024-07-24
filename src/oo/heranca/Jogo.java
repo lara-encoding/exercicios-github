1f1ffb13e729cb712ac3fde325fd0a72e1f3e265
lara.encoding <analara.cafe@gmail.com>
2024-07-24 19:01:02 +0100

    implementando o estudo herança 01.

--- src/oo/heranca/Jogo.java
+++ src/oo/heranca/Jogo.java
@@ -7,15 +7,19 @@ public class Jogo {
 		Jogador j1 = new Jogador();
 		j1.x = 10;
 		j1.y = 10;
+		
+		Jogador j2 = new Jogador();
+		j2.x = 10;
+		j2.y = 11;
 
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
-
-		System.out.println(j1.x);
-		System.out.println(j1.y);
-	
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
+		
+		j1.atacar(j2);
+		
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
+		
 	}
 
 }
