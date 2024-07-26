23ab8dfb80e21f51ecca09a8dc80c9c341f07979
lara.encoding <analara.cafe@gmail.com>
2024-07-26 16:29:00 +0100

    atualizando o estudo de getters e setters.

--- src/oo/composicao/encapsulamento/Pessoa.java
+++ src/oo/composicao/encapsulamento/Pessoa.java
@@ -2,18 +2,51 @@ package oo.composicao.encapsulamento;
 
 public class Pessoa {
 	
+	private String nome;
+	private String sobrenome;
 	private int idade;
 	
-	public Pessoa(int idade) {
-		this.idade = idade;
+	public Pessoa(String nome, String sobrenome, int idade) {
+		setNome(nome);
+		setSobrenome(sobrenome);
+		setIdade(idade);
 	}
 	
-	public int lerIdade() {
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public String getNomeCompleto() {
+		return getNome() + " " + getSobrenome();
+	}
+
+	// Getter
+	public int getIdade() {
 		return idade; 
 	}
 	
-	public void alterarIdade(int novaIdade) {
-		this.idade = novaIdade;
+	// Setter
+	public void setIdade(int novaIdade) {
+		novaIdade = Math.abs(novaIdade);
+		if(novaIdade >= 0 && novaIdade <= 120) {
+			this.idade = novaIdade;
+		}
 	}
 
+	public String toString() {
+		return "Ola eu sou o " + getNome()
+		+ " e tenho " + getIdade() + " anos.";
+	}
 }
--- src/oo/composicao/encapsulamento/PessoaTeste.java
+++ src/oo/composicao/encapsulamento/PessoaTeste.java
@@ -4,10 +4,12 @@ public class PessoaTeste {
 	
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(30);
-		p1.alterarIdade(24); // alterar
+		Pessoa p1 = new Pessoa("Pedro","Amoedo", -45);
+		p1.setIdade(230); // alterar
 		
-		System.out.println(p1.lerIdade()); // ler
+		System.out.println(p1.getIdade()); // ler
+		System.out.println(p1); // toString
+		System.out.println(p1.getNomeCompleto());
 	}
 
 }
